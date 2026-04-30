9ede3c4bcc9cfb2aa20ced16f12a7fb6f67bd381
MizuchiKun <til@jungbluths.de>
2026-04-30 10:23:05 +0200

    Implemented R-, I-, and S-type instructions in ISA specs.
    - Might've also changed something about TRV2602A and Branch0Cond.
      - If so, no clue what.
      - Though I may have just opened them, and that 'modified' them.

--- architecture/TRV2602A/sandbox/new_program.asm
+++ architecture/TRV2602A/sandbox/new_program.asm
@@ -1,3 +1,4 @@
+; For testing RAM hardware stuff (don't quite remember of what).
 U32 0xAABBCCDD
 U32 0x11223344
 U32 0x55555555
@@ -6,3 +7,6 @@ U32 0x55555555
 U32 0x55555555
 U32 0x55555555
 U32 0x55555555
+
+; Instructions to test if CPU is working as intended.
+sb t2, 200(t0)
